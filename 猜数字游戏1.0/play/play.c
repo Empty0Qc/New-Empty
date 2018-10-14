847c044e7e88ba166243302b24e2c176344e22d2
Empty0Qc <43704042+Empty0Qc@users.noreply.github.com>
2018-10-14 18:20:14 +0800

    猜数字游戏
    代码

---
+++ 猜数字游戏1.0/play/play.c
@@ -0,0 +1,61 @@
+
+void game()
+{
+	int ret = 0;
+	int num = 0;
+	ret = rand()%100;
+	while(1)
+	{
+		printf("请猜数字:>");
+		scanf_s("%d",&num);
+		if(num>ret)
+		{
+		printf("猜大了\n");
+		}
+		else if(num<ret)
+		{
+			printf("猜小了\n");
+		}
+		else
+		{
+			printf("恭喜你终于猜对了\n");
+			break;
+		}	
+	}
+	printf("%d\n",ret);
+
+}
+
+void menu()
+{ 
+	printf("*************************\n");
+	printf("*****    1. play    *****\n");
+	printf("*****    0.exit     *****\n");
+	printf("*************************\n");
+}
+int main()
+{
+	int input = 0;
+    srand((unsigned)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf_s("%d", &input);
+		switch(input)
+		{
+		case 1:
+            printf("玩游戏\n");
+			game();
+			break;
+		case 0:
+            printf("退出游戏\n");
+			break;
+        default:
+			printf("选择错误\n");
+			break;		
+		}
+	}
+	while(input);
+	return 0;
+}
