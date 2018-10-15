02199a3369989df9683e95c34474574d88ed3f12
Empty0Qc <43704042+Empty0Qc@users.noreply.github.com>
2018-10-16 00:37:27 +0800

    功能更新
    增加标记和取消标记功能

--- 扫雷/扫雷/game.h
+++ 扫雷/扫雷/game.h
@@ -14,6 +14,7 @@
 #include <windows.h>
 
 void InitBoard(char arr[ROWS][COLS],int row,int col,char set);
+void InitBoard1(char arr1[ROWS][COLS],char arr2[ROWS][COLS],int row,int col);
 void DisplayBoard(char arr[ROWS][COLS],int row,int col);
 void SetMine(char arr[ROWS][COLS],int row,int col,int diff);
 void FindMine(char mine[ROWS][COLS],char show[ROWS][COLS],int row,int col,int diff);
@@ -22,5 +23,7 @@ int MuchMine(char mine[ROWS][COLS],int x,int y);
 void Ass(char mine[ROWS][COLS],char show[ROWS][COLS],int x,int y,int *Num);
 int IsWin(char show[ROWS][COLS],int row,int col,int diff);
 void DisplayBoard1(char arr[ROWS][COLS],int row,int col);
+void menu3();
+void sign(char show[ROWS][COLS],char show2[ROWS][COLS],char x);
 
 #endif //__GAME_H__
