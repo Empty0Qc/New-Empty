c3b79aadb64453e2405cf814d090e1af04ef03d1
Empty0Qc <43704042+Empty0Qc@users.noreply.github.com>
2018-10-27 23:19:54 +0800

    判断几块钱由5分2分1分组成的方法有多少。

---
+++ Test_10_27/Test_10_27/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	int input,m,n5,n2,n1=0,k=0;
+	printf("请输入要知道的钱数:");
+	scanf("%d",&input);
+	m = input*100;
+	for(n5=0; n5<=m/5; n5++)
+	{
+		for(n2=0; n2<=(m-5*n5)/2; n2++)
+		{
+			n1 = m-5*n5-2*n2;
+			if(n1 >= 0)
+			{
+				printf("%d可由%3d个5分 %3d个2分 %3d个1分 组成\n",input,n5,n2,n1);
+							k++;
+			}
+		}
+	}
+	printf("总共有%d种组合方法。\n",k);
+	return 0;
+}
