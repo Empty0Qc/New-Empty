91956a50c31eb26171a57fc5c6f6fac3ff0987a1
Empty0Qc <43704042+Empty0Qc@users.noreply.github.com>
2018-11-03 22:23:28 +0800

    New Code Day15

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,8 +1,9 @@
-  public class HelloWorld
-  {
-	  public static void main(String [] args)
-	  {
-		  System.out.println("hehe");
-	  }
-  }
+
+public class HelloWorld{
+	public static void main(String[] args)
+	{
+		System.out.println("hehe");
+	}  
+}
+  
   
