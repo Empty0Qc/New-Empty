482ff4387e9950baf3ee530c17627be845fb82d3
Empty0Qc <43704042+Empty0Qc@users.noreply.github.com>
2018-11-08 23:21:37 +0800

    1

--- Java/Java_1106.java
+++ Java/Java_1106.java
@@ -1,8 +1,11 @@
 
 public  class Java_1106{
-
-	public static void main(String [] args){
-		char ch1 = 'B';
-		System.out.println(ch1+1);
-	}
+    
+    public static void main(String args[]) {                  
+        String  s1=new String("abc");
+        String  s2=new String("abc"); 
+        boolean b1=s1.equals(s2);
+        boolean b2=(s1==s2);            
+        System.out.print(b1+"   "+b2); 
+    } 	
 }
--- Java/abc.java
+++
@@ -1,5 +0,0 @@
-public class abc{
-	static void main(String []args){
-		System.out.println('A'+1);
-	}
-}
