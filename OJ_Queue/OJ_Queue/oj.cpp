5d3a37431d2f0a787e17d577f927a6152aaf576e
Empty0Qc <43704042+Empty0Qc@users.noreply.github.com>
2019-05-08 21:32:56 +0800

    模板

--- OJ_Queue/OJ_Queue/oj.cpp
+++ OJ_Queue/OJ_Queue/oj.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 //#include <deque>
 #include <queue>
+#include <deque>
 #include <functional>
 
-
+#if 0
 int main()
 {
 	priority_queue<int,vector<int>,greater<int>> pq;
@@ -19,5 +20,15 @@ int main()
 	}
 	cout << endl;
 	return 0;
-
 }
+#endif
+
+#if 1
+
+int main()
+{
+	return 0;
+}
+
+
+#endif
