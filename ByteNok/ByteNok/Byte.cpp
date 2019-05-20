baa7a260b3177869f03e22942b4c349b7f5da529
Empty0Qc <43704042+Empty0Qc@users.noreply.github.com>
2019-05-21 01:05:03 +0800

    oj

--- ByteNok/ByteNok/Byte.cpp
+++ ByteNok/ByteNok/Byte.cpp
@@ -1,63 +1,150 @@
 #include <iostream>
-#include <vector>
+#include <assert.h>
 using namespace std;
 
-class A
+
+#if 1
+
+char* _reverse_all(char* start, char* end)
 {
-public:
-	void fun()
+	assert(start);
+	assert(end);
+	char* p = start;
+	char tmp;
+	while (start < end)
 	{
-		cout << "func()" << endl;
+		tmp = *start;
+		*start = *end;
+		*end = tmp;
+		start++;
+		end--;
 	}
-};
-class B : public A
+	return p;
+}
+
+char* _reverse(char *str)
 {
-public:
-	void fun(int i)
+	assert(str);
+	char *pstr = str;
+	char *start = str;
+	char *end = str;
+	while (*end != '\0')
 	{
-		A::fun();
-		cout << "func(int i)->" << i << endl;
+		while (*end != ' ' && *end != '\0')end++;
+		end--;
+		_reverse_all(start, end);
+		start = end + 2;
+		end = start;
 	}
-};
-void Test()
+	return pstr;
+}
+int main()
 {
-	B b;
-	b.fun(10);
-};
+	char s[10244] = {0};
+	gets(s);
+	_reverse_all(s, s + strlen(s) - 1);
+	printf("%s\n",_reverse(s));
+	return 0;
+}
+#endif
+#if 0
+
+#include <iostream>
+using namespace std;
 
 int main()
 {
-	Test();
+	int n, tmp, count = 0, prev = 0;
+	bool x = true;
+	cin >> n;
+	while (n--)
+	{
+		cin >> tmp;
+		if (x && tmp > prev)
+		{
+			x = false;
+			count++;
+		}
+		else if (!x && tmp < prev)
+		{
+			x = true;
+			count++;
+		}
+		prev = tmp;
+	}
+	cout << count;
 	return 0;
 }
-//
-//bool DoCheck(vector<int> &vv)
+
+#endif
+
+//class A
 //{
-//	if (vv.size() == 0)
-//		return true;
-//	int len = vv.size();
-//	if ()
-//	return false;
-//}
+//public:
+//	void fun()
+//	{
+//		cout << "func()" << endl;
+//	}
+//};
+//class B : public A
+//{
+//public:
+//	void fun(int i)
+//	{
+//		A::fun();
+//		cout << "func(int i)->" << i << endl;
+//	}
+//};
+//void Test()
+//{
+//	B b;
+//	b.fun(10);
+//};
 //
 //int main()
 //{
-//	vector<int> v;
-//	for (int i = 0; i < 13; i++)
-//	{
-//		int tmp;
-//		cin >> tmp;
-//		v.push_back(tmp);
-//	}
-//	for (int i = 1; i < 10; i++)
-//	{
-//		if (DoCheck(v))
-//			cout << i;
-//	}
+//	Test();
 //	return 0;
 //}
+//
 
+#if 0
 
+int count(vector<int> &vvv,int x)
+{
+
+}
+bool DoCheck(vector<int> &vv)
+{
+	if (vv.size() == 0)
+		return true;
+	int len = vv.size();
+	int count0 = count(vv,vv.data);
+	if (len % 3 != 0 && count0 >= 2)
+	{
+
+	}
+	return false;
+}
+
+int main()
+{
+	vector<int> v;
+	for (int i = 0; i < 13; i++)
+	{
+		int tmp;
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+	for (int i = 1; i < 10; i++)
+	{
+		if (DoCheck(v))
+			cout << i;
+	}
+	return 0;
+}
+
+#endif
 //
 //#include <iostream>
 //using namespace std;
