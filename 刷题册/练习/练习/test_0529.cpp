e92c1a979fe4f3341aecdb26cc97f5d0e775209e
Empty0Qc <43704042+Empty0Qc@users.noreply.github.com>
2019-05-30 00:43:10 +0800

    练题_push

--- 刷题册/练习/练习/test_0529.cpp
+++ 刷题册/练习/练习/test_0529.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-using namespace std;
-#define ull unsigned long long
-void jiec(int n)
-{
-	ull sum = 1;
-	for (int i = 1; i <= n; i++)
-	{
-		sum *= i;
-	}
-	cout << sum;
-}
-
-int main()
-{
-	int n;
-	while(cin >> n)
-		jiec(n);
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//#define ull unsigned long long
+//void jiec(int n)
+//{
+//	ull sum = 1;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		sum *= i;
+//	}
+//	cout << sum;
+//}
+//
+//int main()
+//{
+//	int n;
+//	while(cin >> n)
+//		jiec(n);
+//	return 0;
+//}
