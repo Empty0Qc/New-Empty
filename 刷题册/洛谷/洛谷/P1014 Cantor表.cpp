b62853a77e7b927e658011a8ee53fca695843d84
Empty0Qc <43704042+Empty0Qc@users.noreply.github.com>
2019-06-15 20:25:38 +0800

    1101111

---
+++ 刷题册/洛谷/洛谷/P1014 Cantor表.cpp
@@ -0,0 +1,22 @@
+////https://www.luogu.org/problemnew/show/P1014
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	while (cin >> n)
+//	{
+//		int tmp = n,count = 1;
+//		while (tmp > count)
+//		{
+//			tmp -= count;
+//			count++;
+//		}
+//		if (count%2 == 1)
+//			cout << count + 1 - tmp << "/" << tmp << endl;
+//		else
+//			cout << tmp << "/" << count + 1 - tmp << endl;
+//	}
+//	return 0;
+//}
