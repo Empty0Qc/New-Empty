47efd405bf9f4e6ccb769ed0f83bb16c1911ef16
Empty0Qc <43704042+Empty0Qc@users.noreply.github.com>
2019-06-16 22:25:31 +0800

    11010000

---
+++ 刷题册/洛谷/洛谷/P1003 铺地毯.cpp
@@ -0,0 +1,21 @@
+//https://www.luogu.org/problemnew/show/P1003
+//#include <iostream>
+//using namespace std;
+//int n, a[10001], b[10001], c[10001], d[10001], x, y;
+//int main()
+//{
+//	cin >> n;
+//	for (int i = 1; i <= n; i++)
+//		cin >> a[i] >> b[i] >> c[i] >> d[i];
+//	cin >> x >> y;
+//	for (int i = n; i >= 1; i--)
+//	{
+//		if (x >= a[i] && x <= a[i] + c[i] && y >= b[i] && y <= b[i] + d[i])
+//		{
+//			cout << i;
+//			return 0;
+//		}
+//	}
+//	cout << -1;
+//	return 0;
+//}
--- 刷题册/洛谷/洛谷/P1060 开心的金明.cpp
+++ 刷题册/洛谷/洛谷/P1060 开心的金明.cpp
@@ -1,32 +1,32 @@
-//https://www.luogu.org/problemnew/show/P1060
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <algorithm>
-using namespace std;
-
-
-int main()
-{
-	int com, con;
-	while (cin >> com >> con)
-	{
-		int price[30] = { 0 };
-		int wight[30] = { 0 };
-		int f[30005] = { 0 };
-		for (int i = 1; i <= con; i++)
-		{
-			cin >> price[i] >> wight[i];
-			wight[i] *= price[i];
-		}
-		for (int i = 1; i <= con; i++)
-		{
-			for (int j = com; j >= price[i]; j--)
-			{
-				f[j] = max(wight[i] + f[j - price[i]],f[j]);
-			}
-		}
-		cout << f[com] << endl;
-	}
-	return 0;
-}
+////https://www.luogu.org/problemnew/show/P1060
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <algorithm>
+//using namespace std;
+//
+//
+//int main()
+//{
+//	int com, con;
+//	while (cin >> com >> con)
+//	{
+//		int price[30] = { 0 };
+//		int wight[30] = { 0 };
+//		int f[30005] = { 0 };
+//		for (int i = 1; i <= con; i++)
+//		{
+//			cin >> price[i] >> wight[i];
+//			wight[i] *= price[i];
+//		}
+//		for (int i = 1; i <= con; i++)
+//		{
+//			for (int j = com; j >= price[i]; j--)
+//			{
+//				f[j] = max(wight[i] + f[j - price[i]],f[j]);
+//			}
+//		}
+//		cout << f[com] << endl;
+//	}
+//	return 0;
+//}
