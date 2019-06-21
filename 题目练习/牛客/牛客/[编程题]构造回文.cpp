eb0afb17d2d125b781c9e3ed848cdee159ee8f9e
Empty0Qc <43704042+Empty0Qc@users.noreply.github.com>
2019-06-22 00:29:56 +0800

    11010110

--- 题目练习/牛客/牛客/[编程题]构造回文.cpp
+++ 题目练习/牛客/牛客/[编程题]构造回文.cpp
@@ -1,3 +1,49 @@
+//https://www.nowcoder.com/questionTerminal/28c1dc06bc9b4afd957b01acdf046e69
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int Movfun[1024][1024];
+size_t dfs(string &s1, string &s2)
+{
+	size_t l = s1.size();
+	for (size_t i = 0; i <= l; i++)
+		Movfun[i][0] = Movfun[0][i] = 0;
+	for (size_t i = 1; i <= l; i++)
+	{
+		for (size_t j = 1; j <= l; j++)
+		{
+			if (s1[i - 1] == s2[j - 1])
+				Movfun[i][j] = Movfun[i - 1][j - 1] + 1;
+			else
+				Movfun[i][j] = max(Movfun[i - 1][j], Movfun[i][j - 1]);
+		}
+	}
+	return Movfun[l][l];
+}
+int main()
+{
+	string s;
+	while (cin >> s)
+	{
+		string tmp = s;
+		reverse(tmp.begin(), tmp.end());
+		if (s.size() == 1 || s == tmp)
+			cout << 0 << endl;
+		else
+			cout << s.size() - dfs(s,tmp) << endl;
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+//普通方法，时间复杂度过高
 //#include <iostream>
 //#include <string>
 //#include <cstring>
