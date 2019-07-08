8c505f42664276b65136932b62c2cf77804b449e
Empty0Qc <43704042+Empty0Qc@users.noreply.github.com>
2019-07-08 12:09:17 +0800

    1111000

--- 题目练习/牛客/牛客/数字和为sum的方法数.cpp
+++ 题目练习/牛客/牛客/数字和为sum的方法数.cpp
@@ -1,41 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS
 //#include <iostream>
-//#include <vector>
+//#include <cstdio>
 //#include <algorithm>
 //using namespace std;
-//
+//#define ll long long
+//int A[1024] = { 0 };
+//ll dp[1024] = { 1 };
 //int main()
 //{
 //	int n, m;
 //	while (cin >> n >> m)
 //	{
-//		int cnt = 0;
-//		vector<int> v; v.push_back(0);
-//		for (int i = 0; i < n; i++)
-//		{
-//			int t; cin >> t;
-//			if (t <= m)
-//				v.push_back(t);
-//		}
-//		sort(v.begin(),v.end());
-//		n = v.size() - 1;
-//		vector<int> dp(n + 1, 0);
-//		for (int i = 1; i <= n; i++)
-//			dp[i] = v[i];
-// 		for (int i = 1; i <= n; i++)
-//		{
-//			for (int j = i; j <= n; j++)
-//			{
-//				if (i != j)
-//				{
-//					dp[j] = v[i] + dp[j];
-//					if (dp[j] == m)
-//						cnt++;
-//				}
-//				
-//				
-//			}
-//		}
-//		cout << cnt * 2 << endl;
+//		for (int i = 1; i <= n; i++)scanf("%d", &A[i]);
+//		for (int i = 1; i <= n; i++)for (int j = m; j >= A[i]; j--)
+//			dp[j] += dp[j - A[i]];
+//		cout << dp[m] << endl;
 //	}
 //	return 0;
 //}
