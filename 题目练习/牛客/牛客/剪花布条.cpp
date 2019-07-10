17802054450cbeb6e96187c5e108439a57f313b8
Empty0Qc <43704042+Empty0Qc@users.noreply.github.com>
2019-07-10 23:55:54 +0800

    1111010

---
+++ 题目练习/牛客/牛客/剪花布条.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//
+//int main()
+//{
+//	string s1, s2;
+//	while (cin >> s1 >> s2)
+//	{
+//		int start = 0;
+//		int num = 0;
+//		while (start + s2.size() <= s1.size())
+//		{
+//			if (s1.substr(start, s2.size()) == s2)
+//			{
+//				num++;
+//				start += s2.size();
+//			}
+//			else		
+//				start++;
+//		}
+//		cout << num << endl;
+//	}
+//	return 0;
+//}
